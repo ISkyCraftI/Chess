4445023d300ce0aee3aa6aa67c926db1bc841569
ISkyCraftI <133578001+ISkyCraftI@users.noreply.github.com>
2023-10-24 15:21:56 +0200

    Update code.py

--- code.py
+++ code.py
@@ -22,16 +22,40 @@ class Pion(Piece):
 
         # Les pions se déplacent en avant d'une case (ou de deux au premier coup)
         if self.couleur == 'blanc':
-            direction = 1  # Les pions blancs avancent vers le haut du plateau
+            direction = 1                      # Les pions blancs avancent vers le haut du plateau
         else:
-            direction = -1  # Les pions noirs avancent vers le bas du plateau
+            direction = -1                     # Les pions noirs avancent vers le bas du plateau
 
         if self.premier_coup and y2 == y1 + 2 * direction:
-            return True  # Premier coup, peut avancer de deux cases
+            return True                        # Premier coup, peut avancer de deux cases
         elif y2 == y1 + direction:
-            return True  # Avancer d'une case
+            return True                        # Avancer d'une case
 
-        return False  # Mouvement invalide
+        return False                           # Mouvement invalide
+
+class Tour(Piece):
+    def __init__(self, couleur):
+        super().__init__(couleur)
+        self.premier_coup = True  # Pour le déplacement initial
+
+    def est_mouvement_valide(self, nouvelle_position):
+        # Logique spécifique pour les pions
+        x1, y1 = self.position
+        x2, y2 = nouvelle_position
+
+        # Les pions se déplacent en avant d'une case (ou de deux au premier coup)
+        if self.couleur == 'blanc':
+            direction = 1                      # Les pions blancs avancent vers le haut du plateau
+        else:
+            direction = -1                     # Les pions noirs avancent vers le bas du plateau
+
+        if self.premier_coup and y2 == y1 + 2 * direction:
+            return True                        # Premier coup, peut avancer de deux cases
+        elif y2 == y1 + direction:
+            return True                        # Avancer d'une case
+
+        return False                           # Mouvement invalide
+
 
 class Plateau:
     length = ['A','B','C','D','E','F','G','H']  # horizontal du plateau
