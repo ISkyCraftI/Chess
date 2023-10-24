ecff1a189ab9779de76f48018138446cdbf53ec3
M4nu3k3 <148872374+M4nu3k3@users.noreply.github.com>
2023-10-24 14:50:35 +0200

    Update code.py
    yolo

--- code.py
+++ code.py
@@ -1,17 +1,22 @@
 class Piece:
-    def init(self, couleur):
-        self.couleur = couleur
+    def __init__(self, couleur):
+        self.couleur = couleur  # 'blanc' ou 'noir'
+        self.position = None
 
-    def deplacer(self, depart, arrivee):
-        # Logique générale pour le déplacement des pièces
-        pass
+    def deplacer(self, nouvelle_position):
+        self.position = nouvelle_position
+
+    def est_mouvement_valide(self, nouvelle_position):
+        # Cette méthode doit être surchargée par chaque pièce spécifique
+        raise NotImplementedError("La méthode est_mouvement_valide doit être implémentée par chaque pièce.")
 
 class Pion(Piece):
-    def deplacer(self, depart, arrivee):
-        # Logique spécifique pour le déplacement du pion
-        pass
+    def __init__(self, couleur):
+        super().__init__(couleur)
+        self.premier_coup = True  # Pour le déplacement initial
 
-class Tour(Piece):
-    def deplacer(self, depart, arrivee):
-        # Logique spécifique pour le déplacement de la tour
-        pass
+    def est_mouvement_valide(self, nouvelle_position):
+        # Logique spécifique pour les pions
+        # Ici, vous devrez vérifier que le mouvement respecte les règles des pions, comme avancer d'une case ou deux lors du premier coup.
+        # Vous devrez également vérifier que le mouvement est valide par rapport à la disposition actuelle du plateau.
+        raise NotImplementedError("La méthode est_mouvement_valide doit être implémentée pour le pion.")
