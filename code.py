0a20b3ac99bccb90156d0a14d32268d1a11ade5d
ISkyCraftI <133578001+ISkyCraftI@users.noreply.github.com>
2023-10-24 14:38:54 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,17 @@
+class Piece:
+    def init(self, couleur):
+        self.couleur = couleur
 
+    def deplacer(self, depart, arrivee):
+        # Logique générale pour le déplacement des pièces
+        pass
+
+class Pion(Piece):
+    def deplacer(self, depart, arrivee):
+        # Logique spécifique pour le déplacement du pion
+        pass
+
+class Tour(Piece):
+    def deplacer(self, depart, arrivee):
+        # Logique spécifique pour le déplacement de la tour
+        pass
