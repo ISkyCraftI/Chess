a35b817280067dca5f79d6f6f815b323422d9a6b
ISkyCraftI <133578001+ISkyCraftI@users.noreply.github.com>
2023-10-25 11:29:09 +0200

    Update Piece.py

--- Data/Classes/Piece.py
+++ Data/Classes/Piece.py
@@ -19,3 +19,46 @@ class Piece:
                 else:
                     output.append(square)
         return output
+   
+    def get_valid_moves(self, board):
+        output = []
+        for square in self.get_moves(board):
+            if not board.is_in_check(self.color, board_change=[self.pos, square.pos]):
+                output.append(square)
+        return output
+   
+    def move(self, board, square, force=False):
+        for i in board.squares:
+            i.highlight = False
+        if square in self.get_valid_moves(board) or force:
+            prev_square = board.get_square_from_pos(self.pos)
+            self.pos, self.x, self.y = square.pos, square.x, square.y
+            prev_square.occupying_piece = None
+            square.occupying_piece = self
+            board.selected_piece = None
+            self.has_moved = True
+            # Pawn promotion
+            if self.notation == ' ':
+                if self.y == 0 or self.y == 7:
+                    from data.classes.pieces.Queen import Queen
+                    square.occupying_piece = Queen(
+                        (self.x, self.y),
+                        self.color,
+                        board
+                    )
+            # Move rook if king castles
+            if self.notation == 'K':
+                if prev_square.x - self.x == 2:
+                    rook = board.get_piece_from_pos((0, self.y))
+                    rook.move(board, board.get_square_from_pos((3, self.y)), force=True)
+                elif prev_square.x - self.x == -2:
+                    rook = board.get_piece_from_pos((7, self.y))
+                    rook.move(board, board.get_square_from_pos((5, self.y)), force=True)
+            return True
+        else:
+            board.selected_piece = None
+            return False
+
+    # True for all pieces except pawn
+    def attacking_squares(self, board):
+        return self.get_moves(board)
